948e271417a2fb9418681f7fde3d0fd6c03946bc
Fenet-Ab <abilufenet@gmail.com>
2026-05-25 18:29:31 +0300

    auth service implemented

--- src/main/java/com/pos/app/service/AuthService.java
+++ src/main/java/com/pos/app/service/AuthService.java
@@ -1,4 +1,12 @@
 package com.pos.app.service;
 
+import com.pos.app.dto.LoginRequestDTO;
+import com.pos.app.dto.RegisterRequestDTO;
+
 public interface AuthService {
+    String registerManager(RegisterRequestDTO request);
+    String registerCashier(RegisterRequestDTO request);
+    String login(LoginRequestDTO request);
+
+
 }
