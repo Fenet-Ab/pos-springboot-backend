b1449de70eed9d1614aca0390a1499cdc26fa543
Fenet-Ab <abilufenet@gmail.com>
2026-05-25 18:24:17 +0300

    login request implemented

--- src/main/java/com/pos/app/dto/LoginRequestDTO.java
+++ src/main/java/com/pos/app/dto/LoginRequestDTO.java
@@ -1,4 +1,11 @@
 package com.pos.app.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequestDTO {
+
+    @NotBlank
+    private String password;
 }
