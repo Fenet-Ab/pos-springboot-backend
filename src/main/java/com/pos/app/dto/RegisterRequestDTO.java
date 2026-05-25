abdd4bd6ff846b1f0de506c4565fd2339b85859f
Fenet-Ab <abilufenet@gmail.com>
2026-05-25 18:25:25 +0300

    register request dto implemented

--- src/main/java/com/pos/app/dto/RegisterRequestDTO.java
+++ src/main/java/com/pos/app/dto/RegisterRequestDTO.java
@@ -1,4 +1,17 @@
 package com.pos.app.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class RegisterRequestDTO {
+
+    @NotBlank
+    private String fullName;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
 }
