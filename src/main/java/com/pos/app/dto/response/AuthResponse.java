87620f81b7b225f9af5d45bb599f5b70f5966277
Fenet-Ab <abilufenet@gmail.com>
2026-05-30 23:30:31 +0300

    auth response implemented

--- src/main/java/com/pos/app/dto/response/AuthResponse.java
+++ src/main/java/com/pos/app/dto/response/AuthResponse.java
@@ -1,4 +1,13 @@
 package com.pos.app.dto.response;
 
+import lombok.*;
+
+@Data @Builder @AllArgsConstructor @NoArgsConstructor
 public class AuthResponse {
+    private String token;
+    private String username;
+    private String email;
+    private String fullName;
+    private String role;
+    private String message;
 }
