efaad7decedd462447144d4ac2892427a429bfe4
Fenet-Ab <abilufenet@gmail.com>
2026-06-03 01:48:45 +0300

    user repository implemented

--- src/main/java/com/pos/app/repository/UserRepository.java
+++ src/main/java/com/pos/app/repository/UserRepository.java
@@ -13,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByRole(Role role);
 
     boolean existsByUsername(@NotBlank String username);
+    boolean existsByEmail(String email);
 }
