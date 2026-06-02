64aca62476024c60eac966b30e5ec1617b0bb781
Fenet-Ab <abilufenet@gmail.com>
2026-06-02 23:07:00 +0300

    register requestdto implemented

--- src/main/java/com/pos/app/dto/request/RegisterRequestDTO.java
+++ src/main/java/com/pos/app/dto/request/RegisterRequestDTO.java
@@ -1,5 +1,6 @@
-package com.pos.app.dto;
+package com.pos.app.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -11,6 +12,11 @@ public class RegisterRequestDTO {
 
     @NotBlank
     private String username;
+
+    @Email
+    @NotBlank(message="Email is required")
+    private String email;
+
 
     @NotBlank
     private String password;
