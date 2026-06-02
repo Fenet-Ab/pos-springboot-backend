e058418f7ff71147852878085273b67f3e4fa9a0
Fenet-Ab <abilufenet@gmail.com>
2026-06-03 01:46:45 +0300

    register request implemented

--- src/main/java/com/pos/app/dto/request/RegisterRequest.java
+++ src/main/java/com/pos/app/dto/request/RegisterRequest.java
@@ -1,4 +1,27 @@
 package com.pos.app.dto.request;
 
+import com.pos.app.model.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    @NotBlank
+    private String fullName;
+
+    @NotBlank
+    private String username;
+
+    @Email
+    @NotBlank(message="Email is required")
+    private String email;
+
+    @NotNull
+    private Role role;
+
+
+    @NotBlank
+    private String password;
 }
