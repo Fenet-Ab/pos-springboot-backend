9ae16a714eeb3c5881a8be58b1df56e9daf8a367
Fenet-Ab <abilufenet@gmail.com>
2026-06-02 23:10:17 +0300

    user repository implemeted

--- src/main/java/com/pos/app/repository/UserRepository.java
+++ src/main/java/com/pos/app/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.pos.app.repository;
 import com.pos.app.model.entity.User;
+import com.pos.app.model.enums.Role;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+    List<User> findByRole(Role role);
+
+    boolean existsByUsername(@NotBlank String username);
 }
