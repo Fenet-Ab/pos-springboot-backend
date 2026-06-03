183f388bff59da0593df27afc0a9f34e0d511dfd
Fenet-Ab <abilufenet@gmail.com>
2026-06-04 01:32:54 +0300

    change password request implemented

--- src/main/java/com/pos/app/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/pos/app/dto/request/ChangePasswordRequest.java
@@ -1,4 +1,13 @@
 package com.pos.app.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class ChangePasswordRequest {
+    @NotBlank
+    private String currentPassword;
+    @NotBlank
+    private String newPassword;
+
 }
