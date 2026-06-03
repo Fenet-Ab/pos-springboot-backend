302a5c21b1d11e68ce688b559a5c432dbe87ada9
Fenet-Ab <abilufenet@gmail.com>
2026-06-04 01:34:03 +0300

    forgot password request implemented

--- src/main/java/com/pos/app/dto/request/ForgotPasswordRequest.java
+++ src/main/java/com/pos/app/dto/request/ForgotPasswordRequest.java
@@ -1,4 +1,12 @@
 package com.pos.app.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class ForgotPasswordRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
 }
