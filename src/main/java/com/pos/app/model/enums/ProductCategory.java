6563bf8b469cac3725c616e237728f85c783cbe5
Fenet-Ab <abilufenet@gmail.com>
2026-06-05 21:28:16 +0300

    product constan implemented

--- src/main/java/com/pos/app/model/enums/ProductCategory.java
+++ src/main/java/com/pos/app/model/enums/ProductCategory.java
@@ -1,4 +1,12 @@
 package com.pos.app.model.enums;
 
 public enum ProductCategory {
+    FOOD,
+    DRINK,
+    ELECTRONICS,
+    STATIONERY,
+    COSMETICS,
+    BAKERY,
+    BEVERAGES,
+    OTHER,
 }
