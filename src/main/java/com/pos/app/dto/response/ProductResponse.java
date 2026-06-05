97999c5ab6e0a6a7e36d2ff3b1ba5fae9260ba47
Fenet-Ab <abilufenet@gmail.com>
2026-06-05 21:27:17 +0300

    product response implemented

--- src/main/java/com/pos/app/dto/response/ProductResponse.java
+++ src/main/java/com/pos/app/dto/response/ProductResponse.java
@@ -1,4 +1,19 @@
 package com.pos.app.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
 public class ProductResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer quantity;
+    private String imageUrl;
+    private String category;
 }
