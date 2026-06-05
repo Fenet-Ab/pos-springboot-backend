d78c14b91f5da53218dd8be2c0d07ebb4bf17d88
Fenet-Ab <abilufenet@gmail.com>
2026-06-05 21:25:59 +0300

    product controller implemented

--- src/main/java/com/pos/app/controller/ProductController.java
+++ src/main/java/com/pos/app/controller/ProductController.java
@@ -1,4 +1,65 @@
 package com.pos.app.controller;
 
+import com.pos.app.dto.request.ProductRequest;
+import com.pos.app.dto.response.ApiResponse;
+import com.pos.app.dto.response.ProductResponse;
+import com.pos.app.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ApiResponse<ProductResponse> create(
+            @Valid @RequestBody ProductRequest request
+    ) {
+        return ApiResponse.success(
+                "Product created",
+                productService.create(request)
+        );
+    }
+
+    @GetMapping
+    public ApiResponse<List<ProductResponse>> getAll() {
+        return ApiResponse.success(
+                "Products fetched",
+                productService.getAll()
+        );
+    }
+    @GetMapping("/{id}")
+    public ApiResponse<ProductResponse> getById(@PathVariable Long id ) {
+        return ApiResponse.success(
+                "Product found",
+                productService.getById(id)
+        );
+    }
+    @PutMapping("/{id}")
+    public ApiResponse<ProductResponse> update(
+            @PathVariable Long id,
+            @Valid @RequestBody ProductRequest request
+    ) {
+        ProductResponse response =
+                productService.updateById(id, request);
+
+        return ApiResponse.success(
+                "Product updated successfully",
+                response
+        );
+    }
+    @DeleteMapping("/{id}")
+    public ApiResponse<ProductResponse> delete(@PathVariable Long id) {
+        productService.deleteById(id);
+        return ApiResponse.success(
+                "Product deleted successfully",
+                null
+        );
+    }
 }
