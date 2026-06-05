19a35e0df95d2b9750302b1d0cff5d9a65bc2205
Fenet-Ab <abilufenet@gmail.com>
2026-06-05 21:26:34 +0300

    product request dto implemented

--- src/main/java/com/pos/app/dto/request/ProductRequest.java
+++ src/main/java/com/pos/app/dto/request/ProductRequest.java
@@ -1,4 +1,29 @@
 package com.pos.app.dto.request;
 
+import com.pos.app.model.enums.ProductCategory;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class ProductRequest {
+
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    @NotNull
+    @Positive
+    private BigDecimal price;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer quantity;
+
+    private String imageUrl;
+
+    @NotNull
+    private ProductCategory category;
 }
