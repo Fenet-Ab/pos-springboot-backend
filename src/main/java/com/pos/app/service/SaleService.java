cc7cb469e40ff3b7e1c072c42042021c3904a9e3
Fenet-Ab <abilufenet@gmail.com>
2026-06-09 23:22:50 +0300

    salaservice interface implemented

--- src/main/java/com/pos/app/service/SaleService.java
+++ src/main/java/com/pos/app/service/SaleService.java
@@ -1,4 +1,14 @@
 package com.pos.app.service;
+import com.pos.app.dto.request.SaleRequest;
+import com.pos.app.dto.response.SaleResponse;
+import com.pos.app.model.entity.User;
 
-public class SaleService {
+public interface SaleService {
+
+    SaleResponse createSale(
+            SaleRequest request,
+            User cashier
+    );
+
+    SaleResponse verifyPayment(String txRef);
 }
