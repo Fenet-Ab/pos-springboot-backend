ea22997c036a8c6e7ef7951b3a4d21e46dc82551
Fenet-Ab <abilufenet@gmail.com>
2026-06-09 23:16:35 +0300

    sales response implemented

--- src/main/java/com/pos/app/dto/response/SaleResponse.java
+++ src/main/java/com/pos/app/dto/response/SaleResponse.java
@@ -1,4 +1,36 @@
 package com.pos.app.dto.response;
 
+
+import com.pos.app.model.enums.PaymentMethod;
+import com.pos.app.model.enums.PaymentStatus;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SaleResponse {
+
+    private Long saleId;
+
+    private String cashier;
+
+    private PaymentMethod paymentMethod;
+
+    private PaymentStatus paymentStatus;
+
+    private BigDecimal totalAmount;
+
+    private String txRef;
+
+    private String checkoutUrl;
+
+    private LocalDateTime createdAt;
+
+    private List<SaleItemResponse> items;
 }
