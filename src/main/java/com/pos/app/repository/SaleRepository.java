6e796660fec88639c5a0ec394996f2e2b5a60770
Fenet-Ab <abilufenet@gmail.com>
2026-06-09 23:21:18 +0300

    sales repository implemented

--- src/main/java/com/pos/app/repository/SaleRepository.java
+++ src/main/java/com/pos/app/repository/SaleRepository.java
@@ -1,4 +1,10 @@
 package com.pos.app.repository;
 
-public interface SaleRepository {
+import com.pos.app.model.entity.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository
+        extends JpaRepository<Sale, Long> {
+
+    java.util.Optional<Sale> findByTxRef(String txRef);
 }
