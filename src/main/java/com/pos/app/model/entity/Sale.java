f9b0bb3ba7fef98793e15d6db3d1ac8a8d1e94b6
Fenet-Ab <abilufenet@gmail.com>
2026-06-09 23:17:22 +0300

    sale entity implemented

--- src/main/java/com/pos/app/model/entity/Sale.java
+++ src/main/java/com/pos/app/model/entity/Sale.java
@@ -1,4 +1,55 @@
 package com.pos.app.model.entity;
 
+
+
+import com.pos.app.model.enums.PaymentMethod;
+import com.pos.app.model.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "sales")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    private String txRef;
+
+    private String checkoutUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "cashier_id")
+    private User cashier;
+
+    @OneToMany(
+            mappedBy = "sale",
+            cascade = CascadeType.ALL
+    )
+    private List<SaleItem> items;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
 }
