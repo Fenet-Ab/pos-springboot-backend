46d328a125e90ac91d017f9f5cdb331e4bf9fbe8
Fenet-Ab <abilufenet@gmail.com>
2026-06-09 23:18:06 +0300

    sale item entity implemented

--- src/main/java/com/pos/app/model/entity/SaleItem.java
+++ src/main/java/com/pos/app/model/entity/SaleItem.java
@@ -1,4 +1,36 @@
 package com.pos.app.model.entity;
 
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "sale_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SaleItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    private BigDecimal unitPrice;
+
+    private BigDecimal subtotal;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id")
+    private Sale sale;
 }
