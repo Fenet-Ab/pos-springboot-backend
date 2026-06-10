685a2f69cf244f67fc6731fa92986609253dd102
Fenet-Ab <abilufenet@gmail.com>
2026-06-10 22:42:39 +0300

    cart repository implemented

---
+++ src/main/java/com/pos/app/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package com.pos.app.repository;
+
+import com.pos.app.model.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByUserId(Long userId);
+}
