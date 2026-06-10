0eab7b3dda5b5d74eba802b628c10c08416d9fc5
Fenet-Ab <abilufenet@gmail.com>
2026-06-10 22:33:40 +0300

    cart ite response implemented

---
+++ src/main/java/com/pos/app/dto/response/CartItemResponse.java
@@ -0,0 +1,20 @@
+package com.pos.app.dto.response;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItemResponse {
+
+    private Long productId;
+    private String productName;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal subtotal;
+    private Integer availableStock;
+}
