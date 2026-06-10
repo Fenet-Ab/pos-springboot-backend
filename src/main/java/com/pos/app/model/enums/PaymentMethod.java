661a361789bfc136da62a14f85a23367aad6576b
Fenet-Ab <abilufenet@gmail.com>
2026-06-10 22:39:02 +0300

    payment method enums implemented

--- src/main/java/com/pos/app/model/enums/PaymentMethod.java
+++ src/main/java/com/pos/app/model/enums/PaymentMethod.java
@@ -1,6 +1,8 @@
 package com.pos.app.model.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Payment method for orders")
 public enum PaymentMethod {
         CASH,
         TELEBIRR,
